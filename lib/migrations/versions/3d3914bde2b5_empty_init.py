cfb0e7748b7b24a69c7e2dc5ebb9d3d6093198b9
Imelda Hope <imeldalarusso@gmail.com>
2024-09-17 11:52:18 +0300

    creates migrations using alembic

---
+++ lib/migrations/versions/3d3914bde2b5_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3d3914bde2b5
+Revises: 
+Create Date: 2024-09-17 11:37:18.222684
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3d3914bde2b5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
